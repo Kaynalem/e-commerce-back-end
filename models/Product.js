d00337f64be9f3f3aec5ebbe05dba6658dbd914a
Kaynalem <61393721+Kaynalem@users.noreply.github.com>
2020-06-14 09:24:51 -0500

    add models and relationships

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,40 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+  // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        // Validates that the value is a decimal
+        isDecimal: true
+      } 
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        // Validates that the value is a decimal
+        isNumeric: true
+      } 
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
